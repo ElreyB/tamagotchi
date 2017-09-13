ebefe2245586480ff5634eca801c98d8c7843cdc
Elrey Belmonti and LDMacKrell <ldmackrell@>
2017-09-13 15:01:03 -0700

    fix errors in spec test with unresolved fails WIP.

--- lib/tamagotchi.rb
+++ lib/tamagotchi.rb
@@ -1,18 +1,18 @@
-# require('pry')
-#
-# class Tamagotchi
-#   attr_accessor(:name, :food_level, :happiness_level, :awake?)
-#   def initialize(name)
-#     @name = name
-#     @food_level = 10
-#     @happiness_level = 10
-#     @awake = true
-#   end
-#
-#   def is_alive?
-#     @food_level > 0
-#   end
-#   #
-#   # def set_food_level
-#
-# end
+require('pry')
+
+class Tamagotchi
+  attr_accessor(:name, :food_level, :happiness_level, :awake)
+  def initialize(name)
+    @name = name
+    @food_level = 10
+    @happiness_level = 10
+    @awake = true
+  end
+
+  def is_alive?
+    @food_level > 0
+  end
+  #
+  # def set_food_level
+
+end
